c3490fc8f7a0bc1fd5fd7ede8cd0a1c1c1c907b0
Arda Çetinkaya <ctnkyrd@gmail.com>
2017-12-13 08:21:16 +0300

    pg_dump sql formatında database kaydetme komutu eklendi

---
+++ postgresql/pg_dump.sql
@@ -0,0 +1,2 @@
+-- cmd üzerinden postgre 9.3 directory sine girilecek backup.sql almak için kullanılır
+C:\Program Files\PostgreSQL\9.3\bin\pg_dump.exe -U postgres sulama > "C:\Program Files\sulama.sql"
